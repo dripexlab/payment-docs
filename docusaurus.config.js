71f545dfba6bc335648486d66935c089c279c568
J <jsam@Mac.lan>
2025-06-01 17:38:51 +0200

    Fix: cleaned up package.json and Netlify config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/dripexlab/payment-docs.git',
+  url: 'https://dripexlab.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/payment-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
